6830b84916a68d01ec5144a482cb38e6299db859
stronze21 <joshua070915@gmail.com>
2025-03-13 22:54:06 +0800

    Revert "fix"
    This reverts commit e2f8dad86b2bdf4db8bd4c936fd36d285f39c324.

--- resources/views/game-hub.blade.php
+++ resources/views/game-hub.blade.php
@@ -10,7 +10,7 @@
             <div class="flex items-center">
                 <template x-if="currentProfile">
                     <div class="flex items-center">
-                        <img :src="{{ url('/images/avatars/') }} + currentProfile.avatar + '.png'"
+                        <img :src="{ { url('' / images / avatars / ' + currentProfile.avatar + '.png '"
                             class="w-12 h-12 rounded-full border-2 border-purple-400" :alt="currentProfile.name">
                         <div class="ml-3">
                             <h2 class="font-bold text-gray-800" x-text="currentProfile.name"></h2>
@@ -89,7 +89,8 @@
                         </button>
                     </div>
                 </div>
-                <img src="/images/games/number-recognition.png" alt="Number Recognition" class="w-full h-32 object-cover">
+                <img src="{{ url('/images/games/number-recognition.png') }}" alt="Number Recognition"
+                    class="w-full h-32 object-cover">
             </div>
 
             <!-- Counting Game -->
@@ -118,7 +119,7 @@
                         </button>
                     </div>
                 </div>
-                <img src="/images/games/counting-game.png" alt="Counting Game" class="w-full h-32 object-cover">
+                <img src="{{ url('/images/games/counting-game.png') }}"alt="Counting Game" class="w-full h-32 object-cover">
             </div>
 
             <!-- Addition Game -->
@@ -147,7 +148,7 @@
                         </button>
                     </div>
                 </div>
-                <img src="/images/games/addition-game.png" alt="Addition Game" class="w-full h-32 object-cover">
+                <img src="{{ url('/images/games/addition-game.png') }}"alt="Addition Game" class="w-full h-32 object-cover">
             </div>
 
             <!-- Shapes Recognition -->
@@ -176,7 +177,7 @@
                         </button>
                     </div>
                 </div>
-                <img src="/images/games/shapes-game.png" alt="Shapes Game" class="w-full h-32 object-cover">
+                <img src="{{ url('/images/games/shapes-game.png') }}"alt="Shapes Game" class="w-full h-32 object-cover">
             </div>
 
             <!-- Subtraction Game -->
@@ -205,7 +206,8 @@
                         </button>
                     </div>
                 </div>
-                <img src="/images/games/subtraction-game.png" alt="Subtraction Game" class="w-full h-32 object-cover">
+                <img src="{{ url('/images/games/subtraction-game.png') }}"alt="Subtraction Game"
+                    class="w-full h-32 object-cover">
             </div>
 
             <!-- Measurement Game -->
@@ -235,7 +237,7 @@
                         </button>
                     </div>
                 </div>
-                <img src="/images/games/measurement-game.png" alt="Measurement Game"
+                <img src="{{ url('/images/games/measurement-game.png') }}"alt="Measurement Game"
                     class="w-full h-32 object-cover opacity-75">
             </div> --}}
         </div>
